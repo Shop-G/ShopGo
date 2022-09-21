a029c154ce3a646b9e39ea7faab4df88f8df5a6f
Anosha Khan <anoshakhan268@gmail.com>
2022-09-21 14:34:28 +0500

    [TS-9]: build login screen

---
+++ ShopGo/logInViewController.swift
@@ -0,0 +1,42 @@
+//
+//  logInViewController.swift
+//  ShopGo
+//
+//  Created by Anoosha Wahid  on 20/09/2022.
+//
+
+import UIKit
+
+class logInViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneNoTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nextButton.layer.borderWidth = 1.0
+        nextButton.layer.cornerRadius = 22
+        nextButton.clipsToBounds = true
+        
+//        let userNameImage = UIImage(named: "")
+//        addLeftImage(textField: usernameTextField, image: userNameImage!)
+        let emailImage = UIImage(named: "mail")
+        addLeftImage(textField: emailTextField, image: emailImage!)
+//       let phoneNoImage = UIImage(named: "phone")
+//       addLeftImage(textField: phoneNoTextField, image: phoneNoImage!)
+        let passwordImage = UIImage(named: "password")
+        addLeftImage(textField: passwordTextField, image: passwordImage!)
+    }
+    
+    func addLeftImage(textField: UITextField , image: UIImage) {
+        let leftImageView = UIImageView(frame: CGRect(x: 0.0, y: 0.0, width: image.size.width, height: image.size.height))
+        leftImageView.image = image
+        textField.leftView = leftImageView
+        textField.leftViewMode = .always
+    }
+   
+
+}
