01718c406a96d3322ae44591d101d553ae3cfac5
Muhammad A Rehman <iosmuhammadrehman@gmail.com>
2022-09-22 17:40:59 -0400

    build otp screen

---
+++ ShopGo/OtpScreenViewController.swift
@@ -0,0 +1,92 @@
+//
+//  OtpScreenViewController.swift
+//  ShopGo
+//
+//  Created by Muhammad Rehman on 9/22/22.
+//
+
+import UIKit
+
+class OtpScreenViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textFieldOne: UITextField!
+    @IBOutlet weak var textFieldTwo: UITextField!
+    @IBOutlet weak var textFieldThree: UITextField!
+    @IBOutlet weak var textFieldFour: UITextField!
+    @IBOutlet weak var createAccountButtonOutlet: UIButton!
+    
+    @IBAction func CreateAccountButton(_ sender: Any) {
+        /*
+         Code for Create Account screen
+         */
+    }
+    
+    @IBAction func logInAccountButton(_ sender: Any) {
+        /*
+         Code for login screen
+         */
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textFieldDelegate()
+        createAccountButtonEditing()
+    }
+    
+    //  function for cornerradius and shadow for the create account button
+    func createAccountButtonEditing(){
+        createAccountButtonOutlet.layer.cornerRadius = 22
+        createAccountButtonOutlet.layer.shadowColor = UIColor.black.cgColor
+        createAccountButtonOutlet.layer.shadowOffset = CGSize(width: 5, height: 9)
+        createAccountButtonOutlet.layer.shadowRadius = 10
+        createAccountButtonOutlet.layer.shadowOpacity = 0.4
+    }
+    //    assignig delegates for OTP text fields
+    func textFieldDelegate(){
+        textFieldOne.delegate = self
+        textFieldTwo.delegate = self
+        textFieldThree.delegate = self
+        textFieldFour.delegate = self
+        textFieldOne.becomeFirstResponder()
+    }
+    
+    //  functionality for the textfield
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if ((textField.text?.count)! < 1 ) && (string.count > 0) {
+            if textField == textFieldOne {
+                textFieldTwo.becomeFirstResponder()
+            }
+            if textField == textFieldTwo {
+                textFieldThree.becomeFirstResponder()
+            }
+            if textField == textFieldThree {
+                textFieldFour.becomeFirstResponder()
+            }
+            if textField == textFieldFour {
+                textFieldFour.resignFirstResponder()
+            }
+            textField.text = string
+            return false
+        } else if ((textField.text?.count)! >= 1) && (string.count == 0) {
+            if textField == textFieldTwo {
+                textFieldOne.becomeFirstResponder()
+            }
+            if textField == textFieldThree {
+                textFieldTwo.becomeFirstResponder()
+            }
+            
+            if textField == textFieldFour {
+                textFieldThree.becomeFirstResponder()
+            }
+            if textField == textFieldOne {
+                textFieldOne.resignFirstResponder()
+            }
+            textField.text = ""
+            return false
+        } else if (textField.text?.count)! >= 1 {
+            textField.text = ""
+            return false
+        }
+        return true
+    }
+}
