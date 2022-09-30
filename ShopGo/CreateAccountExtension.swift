62922f4c49904ec0c13a0586a9bf52007ed83d73
Anosha Khan <anoshakhan268@gmail.com>
2022-10-01 01:03:40 +0500

    Create  Account screen login extension updated

--- ShopGo/CreateAccountExtension.swift
+++ ShopGo/CreateAccountExtension.swift
@@ -6,12 +6,34 @@
 //
 import Foundation
 import UIKit
-
-extension  UIViewController  {
+extension  CreateScreenViewController : UITextFieldDelegate {
+    
+    // Setting the custom button corner radius and shadows
+    func updateLayerProperties() {
+        nextButton.layer.cornerRadius = 22
+        nextButton.clipsToBounds = true
+        nextButton.layer.masksToBounds = false
+        nextButton.layer.shadowColor = UIColor.gray.cgColor
+        nextButton.layer.shadowOffset = CGSize(width: 3, height: 3)
+        nextButton.layer.shadowRadius = 10
+        nextButton.layer.shadowOpacity = 1.0
+    }
+    
+    // textField image setup
+    func addLeftImage(textField: UITextField , image: UIImage) {
+        let leftImage = UIImageView()
+        leftImage.image = image
+        let contentView = UIView()
+        contentView.addSubview(leftImage)
+        contentView.frame = CGRectMake(0, 0, 25, 20)
+        leftImage.frame = CGRectMake(10, 0, 25, 20)
+        textField.leftView = contentView
+        textField.leftViewMode = .always
+    }
     
     //Hide keyboard
     func hideKeyboardTappedAccount() {
-        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(CreateScreenViewController.dismissKeyboard))
         tap.cancelsTouchesInView = false
         view.addGestureRecognizer(tap)
     }
@@ -20,34 +42,67 @@ extension  UIViewController  {
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
-}
-extension String {
+    
+    // dismiss the keyboard on return tap
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
     
     //Username validation
-    var isValidUsername: Bool {
+    func isValidUsername(username: String) -> Bool {
         let usernameRegEx = "\\w{5,18}"
         let usernameTest = NSPredicate(format: "SELF MATCHES %@", usernameRegEx)
-        return usernameTest.evaluate(with: self)
+        return usernameTest.evaluate(with: username)
     }
-
+    
     //Password validation
-    var isValidPassword: Bool {
+    func isValidPassword(password: String) -> Bool {
         let passwordRegEx = "(?=.*\\d)(?=.*[d$@$!%*?&#])[A-Za-z\\dd$@$!%*?&#]{7,}"
         let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
-        return passwordTest.evaluate(with: self)
+        return passwordTest.evaluate(with: password)
     }
     
     //Email Validation
-    var isValidEmail: Bool {
+    func  isValidEmail(email: String) -> Bool {
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{3}"
         let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
-        return emailTest.evaluate(with: self)
+        return emailTest.evaluate(with: email)
     }
     
     // Phone Number Validation
-    var isValidPhoneNo: Bool {
+    func isValidPhoneNo(phoneNo: String) -> Bool {
         let phoneNoRegEx = "^[+]?+[0-9]{5,16}$"
         let phoneNoTest = NSPredicate(format: "SELF MATCHES %@", phoneNoRegEx)
-        return phoneNoTest.evaluate(with: self)
+        return phoneNoTest.evaluate(with: phoneNo)
+    }
+    
+    // enable and disable button
+    func checkForValidForm() {
+        if let name = usernameTextField.text ,
+           let email = emailTextField.text ,
+           let phoneNo = phoneNoTextField.text ,
+           let password = passwordTextField.text
+        {
+            if (isValidUsername(username: name)) && (isValidEmail(email: email)) && (isValidPhoneNo(phoneNo: phoneNo)) && (isValidPassword(password: password)) {
+                nextButton.isEnabled = true
+                nextButton.alpha = 1.0
+            } else {
+                nextButton.isEnabled = false
+                nextButton.alpha = 0.5
+            }
+        }
+    }
+    
+    // Reset the storyboard
+    func resetScreen() {
+        nextButton.isEnabled = false
+        nextButton.alpha = 0.5
+        
+        usernameTextField.text = ""
+        emailTextField.text = ""
+        phoneNoTextField.text = ""
+        passwordTextField.text = ""
     }
 }
+
--- ShopGo/CreateScreenViewController.swift
+++ ShopGo/CreateScreenViewController.swift
@@ -5,8 +5,7 @@
 //  Created by Anoosha Wahid  on 21/09/2022.
 
 import UIKit
-
-class CreateScreenViewController: UIViewController , UITextFieldDelegate {
+class CreateScreenViewController: UIViewController  {
     
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
@@ -17,13 +16,16 @@ class CreateScreenViewController: UIViewController , UITextFieldDelegate {
     let passwordImage = UIImage(named: "password")
     let phoneNoImage = UIImage(named: "phone")
     let userNameImage = UIImage(named: "person")
+    var name = "user"
+    var email = "user@gmail.com"
+    var phone = "+12345678910"
+    var password = "userr5*"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         resetScreen()
         updateLayerProperties()
-        self.hideKeyboardTappedAccount()
-        
+        hideKeyboardTappedAccount()
         addLeftImage(textField: emailTextField, image: emailImage!)
         addLeftImage(textField: passwordTextField, image: passwordImage!)
         addLeftImage(textField: usernameTextField, image: userNameImage!)
@@ -32,16 +34,6 @@ class CreateScreenViewController: UIViewController , UITextFieldDelegate {
         // Listen the keyboards events
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
-    
-    func resetScreen() {
-        nextButton.isEnabled = false
-        nextButton.alpha = 0.5
-        
-        usernameTextField.text = ""
-        emailTextField.text = ""
-        phoneNoTextField.text = ""
-        passwordTextField.text = ""
     }
     
     // Move view 150 points upward
@@ -54,81 +46,43 @@ class CreateScreenViewController: UIViewController , UITextFieldDelegate {
         self.view.frame.origin.y = 0
     }
     
-    // dismiss the keyboard on return tap
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    // Setting the custom button corner radius and shadows
-    func updateLayerProperties() {
-        nextButton.layer.cornerRadius = 22
-        nextButton.clipsToBounds = true
-        nextButton.layer.masksToBounds = false
-        nextButton.layer.shadowColor = UIColor.gray.cgColor
-        nextButton.layer.shadowOffset = CGSize(width: 3, height: 3)
-        nextButton.layer.shadowRadius = 10
-        nextButton.layer.shadowOpacity = 1.0
-    }
-    
-    // textField image setup
-    func addLeftImage(textField: UITextField , image: UIImage) {
-        let leftImageView = UIImageView(frame: CGRect(x: 0.0, y: 0.0, width: image.size.width, height: image.size.height))
-        leftImageView.image = image
-        textField.leftView = leftImageView
-        textField.leftViewMode = .always
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        let usernameField = usernameTextField.text
+        let emailField = emailTextField.text
+        let phoneNoField = phoneNoTextField.text
+        let passwordField = passwordTextField.text
+        if (isValidUsername(username: usernameField!)) && usernameField == name && (isValidEmail(email: emailField!)) && emailField == email && (isValidPhoneNo(phoneNo: phoneNoField!)) && phoneNoField == phone &&
+            (isValidPassword(password: passwordField!)) && passwordField == password {
+            
+        } else {
+            
+        }
+        resetScreen()
     }
     
     // name validation
     @IBAction func nameChanged(_ sender: Any) {
-        if (usernameTextField.text?.isValidUsername)! {
-            checkForValidForm()
-        }
+        checkForValidForm()
     }
     
     // email validation
     @IBAction func emailChanged(_ sender: Any) {
-        if (emailTextField.text?.isValidEmail)! {
-            checkForValidForm()
-        }
+        checkForValidForm()
     }
-        
+    
     // phone number validation
     @IBAction func phoneNoChanged(_ sender: Any) {
-        if (phoneNoTextField.text?.isValidPhoneNo)! {
-            checkForValidForm()
-        }
+        checkForValidForm()
     }
-        
+    
     // password validation
     @IBAction func passwordChanged(_ sender: Any) {
-        if (passwordTextField.text?.isValidPassword)! {
-            checkForValidForm()
-        }
+        checkForValidForm()
     }
-    
-    
-   @IBAction func nextButtonTapped(_ sender: Any) {
-            resetScreen()
-        }
-        
-        // enable and disable button when textfields are empty or not
-    func checkForValidForm() {
-         if let name = usernameTextField.text ,
-               let email = emailTextField.text ,
-               let phoneNo = phoneNoTextField.text ,
-               let password = passwordTextField.text
-            {
-                if !name.isEmpty && !email.isEmpty && !phoneNo.isEmpty && !password.isEmpty {
-                    nextButton.isEnabled = true
-                    nextButton.alpha = 1.0
-                } else {
-                    nextButton.isEnabled = false
-                    nextButton.alpha = 0.5
-                }
-            }
-        }
-    }
+}
+
+
+
 
 
 
