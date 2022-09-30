7058fb93ed0de6590473be3d99711bedd8d55f42
Zuhair Hussain Naqvi <zuhairnaqvi8@gmail.com>
2022-09-30 15:31:35 +0500

    build login screen logic issues resolved

--- ShopGo/LoginVC.swift
+++ ShopGo/LoginVC.swift
@@ -5,7 +5,7 @@
 //
 
 import UIKit
-class LoginVC: UIViewController , UITextFieldDelegate {
+class LoginVC: UIViewController {
     
     var username = "admin"
     var password = "password1@"
@@ -30,14 +30,12 @@ class LoginVC: UIViewController , UITextFieldDelegate {
     @IBAction func loginAction(_ sender: Any) {
         let passwordField = passwordTextfield.text
         let usernameField = usernameTextfield.text
-        if (isValidPassword(password: passwordField!)) && passwordField == password{
+        
+        if (isValidPassword(password: passwordField!)) && passwordField == password && isValidUsername(username: usernameField!) && usernameField == username{
+            
         } else {
-            passwordTextfield.isError(baseColor: UIColor.gray.cgColor, numberOfShakes: 4, revert: true)
+            isError(baseColor: UIColor.gray.cgColor, numberOfShakes: 4.0, revert: true, textfield: passwordTextfield)
             redUnderlineView.isHidden = false
-        }
-        if (isValidUsername(username: usernameField!)) && usernameField == username {
-        } else {
-            usernameTextfield.isError(baseColor: UIColor.gray.cgColor, numberOfShakes: 4, revert: true)
         }
         resetForm()
     }
@@ -51,79 +49,9 @@ class LoginVC: UIViewController , UITextFieldDelegate {
         redUnderlineView.isHidden = true
     }
     
-    // Configuring textfield icons  and padding
-    func configureTextfieldIcons() {
-        guard let usernameImage = UIImage(named: "user") else {return}
-        self.addLeftImageToTextfield(textfield: usernameTextfield, addImage: usernameImage)
-        guard let passwordImage = UIImage(named: "padlock") else {return}
-        self.addLeftImageToTextfield(textfield: passwordTextfield, addImage: passwordImage)
-    }
-    
-    // Dismiss keyboard with return key
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         print("return")
         return true
-    }
-}
-
-extension LoginVC {
-    
-    // Adding a left icon to the textfield with padding
-    func addLeftImageToTextfield(textfield:UITextField , addImage img:UIImage) {
-        let leftImage = UIImageView()
-        leftImage.image = img
-        let contentView = UIView()
-        contentView.addSubview(leftImage)
-        contentView.frame = CGRectMake(0, 0, 25, 20)
-        leftImage.frame = CGRectMake(10, 0, 25, 20)
-        textfield.leftView = contentView
-        textfield.leftViewMode = .always
-    }
-    
-    // Username and Password Validation
-    func resetForm(){
-        usernameTextfield.text = ""
-        passwordTextfield.text = ""
-    }
-    
-    //Button enable and Disable
-    func buttonEnableDisable() {
-        if let name = usernameTextfield.text, let password = passwordTextfield.text {
-            if (isValidPassword(password: password)) && (isValidUsername(username: name)) {
-                loginButton.isEnabled = true
-                loginButton.alpha = 1
-            } else {
-                loginButton.isEnabled = false
-                loginButton.alpha = 0.9
-            }
-        }
-    }
-    
-    // Password validation check
-    func isValidPassword (password : String) -> Bool{
-        let passwordRegEx = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{6,12}$"
-        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
-        let result = passwordTest.evaluate(with: password)
-        return result
-    }
-    
-    //Username validation check
-    func isValidUsername (username : String) -> Bool{
-        let usernameRegEx = "\\w{5,18}"
-        let usernameTest = NSPredicate(format: "SELF MATCHES %@", usernameRegEx)
-        let result = usernameTest.evaluate(with: username)
-        return result
-    }
-    
-    // hide keyboard on tap
-    func hideKeyboardWhenTappedAround() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(LoginVC.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-    
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
     }
 }
