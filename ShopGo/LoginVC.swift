464582045d5b8da303316ef19652ff1a8f84d753
Muhammad A Rehman <iosmuhammadrehman@gmail.com>
2022-10-12 08:54:42 -0400

    build tab bar controller

--- ShopGo/LoginVC.swift
+++ ShopGo/LoginVC.swift
@@ -24,7 +24,7 @@ class LoginVC: UIViewController {
         configureTextfieldIcons()
         resetForm()
         redUnderlineView.isHidden = true
-        hideKeyboardWhenTappedAround()   
+        hideKeyboardWhenTappedAround()
     }
     
     @IBAction func loginAction(_ sender: Any) {
@@ -32,10 +32,8 @@ class LoginVC: UIViewController {
         let usernameField = usernameTextfield.text
         
         if (isValidPassword(password: passwordField!)) && passwordField == password && isValidUsername(username: usernameField!) && usernameField == username{
-            let st = UIStoryboard(name: "HomePage", bundle: nil)
-            let vc = st.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
-            navigationController?.pushViewController(vc, animated: true)
-            
+            tabBar()
+
         } else {
             isError(baseColor: UIColor.gray.cgColor, numberOfShakes: 4.0, revert: true, textfield: passwordTextfield)
             redUnderlineView.isHidden = false
@@ -56,5 +54,41 @@ class LoginVC: UIViewController {
         textField.resignFirstResponder()
         print("return")
         return true
+    }
+    func tabBar(){
+        let tabBarVC = UITabBarController()
+        let vcOne = UIStoryboard(name: "HomePage", bundle: nil).instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        let vcTwo =  UIStoryboard(name: "Cart", bundle: nil).instantiateViewController(withIdentifier: "CartViewController") as! CartViewController
+        let vcThree = UIStoryboard(name: "MyProfileScreen", bundle: nil).instantiateViewController(withIdentifier: "MyProfileTableViewController") as! MyProfileTableViewController
+        vcOne.title = "Home"
+        vcTwo.title = "Cart"
+        vcThree.title = "Profile"
+        tabBarVC.setViewControllers([vcOne,vcTwo,vcThree], animated: false)
+        guard let items = tabBarVC.tabBar.items else {return}
+        let images = ["house","cart"]
+        for i in 0...items.count-2 {
+            items[i].image = UIImage(systemName: images[i])
+        }
+        items[2].image = UIImage(named: "profileTabBarButton")
+        tabBarVC.modalPresentationStyle = .fullScreen
+        present(tabBarVC, animated: true)
+        tabBarApperence(tabBarVC: tabBarVC)
+    }
+    func tabBarApperence(tabBarVC : UITabBarController) {
+        var tabBarheight = tabBarVC.tabBar.frame
+        let tabBarItemAppearance = UITabBarItemAppearance()
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarVC.tabBar.layer.masksToBounds = true
+        tabBarheight.size.height = 20
+        tabBarVC.tabBar.layer.cornerRadius = 25
+        tabBarVC.tabBar.backgroundColor = UIColor(named: "ButtonColor")
+        tabBarVC.tabBar.alpha = 10
+        tabBarItemAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+        tabBarItemAppearance.normal.iconColor = UIColor.black
+        tabBarItemAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(named: "ButtonColor")!]
+        tabBarItemAppearance.selected.iconColor = UIColor(named: "ButtonColor")!
+        tabBarAppearance.stackedLayoutAppearance = tabBarItemAppearance
+        tabBarVC.tabBar.standardAppearance = tabBarAppearance
+        tabBarVC.tabBar.scrollEdgeAppearance = tabBarAppearance
     }
 }
