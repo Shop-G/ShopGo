ac0a051e82c1934337ced9aea1ebbff9364cb954
Blall Ahmad <chahmadblall@gmail.com>
2022-10-18 05:11:06 +0500

    build menu button

--- ShopGo/HomeViewController.swift
+++ ShopGo/HomeViewController.swift
@@ -8,20 +8,22 @@
 import UIKit
 class HomeViewController: UIViewController {
     
-    @IBOutlet weak var locationIcon: UIButton!
-    @IBOutlet weak var burgerMenu: UIButton!
+    var arrayMenu: [MenuButtonDataModel] = [MenuButtonDataModel(option: "Home", image: UIImage(named: "homeIcon")!),
+                                            MenuButtonDataModel(option: "Explore", image: UIImage(named: "Explore")!),
+                                            MenuButtonDataModel(option: "Cart", image: UIImage(named: "ShoppingCart")!),
+                                            MenuButtonDataModel(option: "Stores", image: UIImage(named: "store")!),
+                                            MenuButtonDataModel(option: "Card Details", image: UIImage(named: "card")!),
+                                            MenuButtonDataModel(option: "WishList", image: UIImage(named: "wishList")!)]
+    
+    var isSideViewOpen:Bool = true
     @IBOutlet weak var typeText: UITextField!
     @IBOutlet weak var trendingOption: UIButton!
     @IBOutlet weak var menOption: UIButton!
     @IBOutlet weak var womenOption: UIButton!
     @IBOutlet weak var childrenOption: UIButton!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        placeLeftImageToTextField()
-        modification()
-        self.navigationItem.setHidesBackButton(true, animated: false)
-    }
+    @IBOutlet weak var sideView: UIView!
+    @IBOutlet weak var tableViewSideBar: UITableView!
     override var prefersStatusBarHidden: Bool {
         return true
     }
@@ -33,6 +35,39 @@ class HomeViewController: UIViewController {
     }
     @IBAction func childrenBtn(_ sender: Any) {
     }
+    @IBAction func menuBtn(_ sender: Any) {
+        showBurgerMenu()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        placeLeftImageToTextField()
+        modification()
+        self.navigationItem.setHidesBackButton(true, animated: false)
+        sideView.isHidden = true
+        isSideViewOpen = false
+    }
 }
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayMenu.count
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! HomePageMenuCell
+        cell.nameDisplay.text = arrayMenu[indexPath.row].option
+        cell.imageDisplay.image = arrayMenu[indexPath.row].image
+        print(indexPath.row)
+        return cell
+    }
+    //    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //        if indexPath.row == 5{
+    //            let vc = storyboard?.instantiateViewController(withIdentifier: "wishListViewController") as! wishListViewController
+    //            navigationController?.pushViewController(vc, animated: true)
+    //        }
+}
+
 
 
---
+++ ShopGo/MenuButtonDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  MenuButtonDataModel.swift
+//  ShopGo
+//
+//  Created by Blall Ahmad on 10/18/22.
+//
+
+import Foundation
+import UIKit
+struct MenuButtonDataModel {
+    var option:String
+    var image:UIImage
+}
